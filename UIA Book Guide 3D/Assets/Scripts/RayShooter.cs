06fa6f59bdabfdcfbbb5542095a189e2d29ec369
Hakim McMorris <hakimmcmorris22@berkeley.edu>
2020-12-23 20:49:12 -0800

    Finished Enemy Functionality and Player shooting
    Wandering Enemy AI currently working with no problems. Added shooting for the player.

---
+++ UIA Book Guide 3D/Assets/Scripts/RayShooter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+    Camera _camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnGUI()
+    {
+        int size = 12;
+        float posX = (_camera.pixelWidth / 2) - (size / 4);
+        float posY = (_camera.pixelHeight / 2) - (size / 2);
+        GUI.Label(new Rect(posX, posY, size, size), "*");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+            Ray ray = _camera.ScreenPointToRay(point);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Hit " + hit.point);
+                GameObject hitObject = hit.transform.gameObject;
+                ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
+                if (target != null)
+                {
+                    target.ReactToHit();
+                } else
+                {
+                    StartCoroutine(SphereIndicator(hit.point));
+
+                }
+            }
+        }
+    }
+
+    IEnumerator SphereIndicator(Vector3 position)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = position;
+
+        yield return new WaitForSeconds(2);
+
+        Destroy(sphere);
+    }
+}
