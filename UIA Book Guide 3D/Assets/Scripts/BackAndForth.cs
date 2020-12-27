139b89584e43f073e3ebc6c260ba5cc920b2531d
Hakim McMorris <hakimmcmorris22@berkeley.edu>
2020-12-26 17:58:59 -0800

    Chapter 4 Finished
    A lot of texture work was done in this chapter. Not super exciting but useful. Got to use blender to make a bench and learned about skyboxes.

---
+++ UIA Book Guide 3D/Assets/Scripts/BackAndForth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForth : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float minZ = -16.0f;
+    public float maxZ = 16.0f;
+
+    int _direction = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+
+        bool bounced = false;
+        if (transform.position.z < minZ || transform.position.z > maxZ)
+        {
+            bounced = true;
+            _direction = -_direction;
+        }
+
+        if (bounced)
+        {
+            transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+        }
+    }
+}
